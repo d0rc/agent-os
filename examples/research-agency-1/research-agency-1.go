f8f2180814fe810b80502953ea1fd9f5cab0f248
ds <7993863@gmail.com>
2023-11-16 23:58:14 +0100

    [WiP] improper parsing of agent's output

--- examples/research-agency-1/research-agency-1.go
+++ examples/research-agency-1/research-agency-1.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/d0rc/agent-os/agency"
+	"github.com/d0rc/agent-os/cmds"
 	"github.com/d0rc/agent-os/engines"
 	os_client "github.com/d0rc/agent-os/os-client"
 	"github.com/d0rc/agent-os/utils"
@@ -52,6 +53,7 @@ func main() {
 			continue
 		}
 
+		clientRequests := make([]*cmds.ClientRequest, 0)
 		for _, res := range results {
 			parsedResults, _ := agentState.ParseResponse(res.Content)
 			//fmt.Printf("[%d] %s\n", currentDepth, aurora.BrightGreen(res.Content))
@@ -65,12 +67,42 @@ func main() {
 					fmt.Printf("[%d] command: %s, args: %v\n", currentDepth,
 						aurora.BrightYellow(v.(map[string]interface{})["name"]),
 						aurora.BrightWhite(string(argsJson)))
+					clientRequests = append(clientRequests,
+						getServerCommand(v.(map[string]interface{})["name"].(string), v.(map[string]interface{})["args"].(map[string]interface{})))
 				}
 			}
+		}
+
+		if len(clientRequests) > 0 {
+			fmt.Printf("Sending %d client requests to server: %v\n", len(clientRequests), clientRequests)
 		}
 
 		currentDepth++
 	}
 
 	fmt.Printf("Done in %v\n", time.Since(ts))
+}
+
+func getServerCommand(commandName string, args map[string]interface{}) *cmds.ClientRequest {
+	clientRequest := &cmds.ClientRequest{
+		GoogleSearchRequests: make([]cmds.GoogleSearchRequest, 0),
+	}
+	switch commandName {
+	case "bing-search":
+		keywords := args["keywords"]
+		switch keywords.(type) {
+		case string:
+			clientRequest.GoogleSearchRequests = append(clientRequest.GoogleSearchRequests, cmds.GoogleSearchRequest{
+				Keywords: keywords.(string),
+			})
+		case []interface{}:
+			for _, keyword := range keywords.([]interface{}) {
+				clientRequest.GoogleSearchRequests = append(clientRequest.GoogleSearchRequests, cmds.GoogleSearchRequest{
+					Keywords: keyword.(string),
+				})
+			}
+		}
+	}
+
+	return clientRequest
 }
