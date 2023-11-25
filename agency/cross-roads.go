9ad7eabcb04c667208f17401efab160ff1b00b61
ds <7993863@gmail.com>
2023-11-25 20:55:47 +0000

    [WiP] cross-roads threshold is 5.0

--- agency/cross-roads.go
+++ agency/cross-roads.go
@@ -1,1 +1,54 @@
 package agency
+
+import (
+	"fmt"
+	"github.com/d0rc/agent-os/engines"
+	"strings"
+)
+
+func (agentState *GeneralAgentInfo) applyCrossRoadsMagic(options []*engines.Message) []*engines.Message {
+	allMessagesFromAssistant := true
+	for _, msg := range options {
+		if msg.Role != engines.ChatRoleAssistant {
+			allMessagesFromAssistant = false
+			break
+		}
+	}
+
+	if !allMessagesFromAssistant {
+		// no obvious policy to choose from
+		return options
+	}
+
+	prompt := agentState.Settings.Agent.PromptBased.Prompt
+	promptLines := strings.Split(prompt, "\n")
+	initialGoal := promptLines[0]
+	messageRatings := make([]float32, len(options))
+
+	for i, msg := range options {
+		rating, err := agentState.VoteForAction(initialGoal, msg.Content)
+		if err != nil {
+			fmt.Printf("Error voting for action: %v\n", err)
+			continue
+		}
+
+		messageRatings[i] = rating
+		fmt.Printf("Vote for message %d of %d finished with rating: %f\n", i, len(options), rating)
+	}
+
+	// now calculate the average rating
+	averageRating := float32(0.0)
+	for _, rating := range messageRatings {
+		averageRating += rating
+	}
+	averageRating /= float32(len(messageRatings))
+
+	filteredOptions := make([]*engines.Message, 0)
+	for i, msg := range options {
+		if messageRatings[i] >= 5.0 {
+			filteredOptions = append(filteredOptions, msg)
+		}
+	}
+
+	return filteredOptions
+}
--- agency/general-agent.go
+++ agency/general-agent.go
@@ -348,53 +348,6 @@ func (agentState *GeneralAgentInfo) GeneralAgentPipelineRun(
 	return nil
 }
 
-func (agentState *GeneralAgentInfo) applyCrossRoadsMagic(options []*engines.Message) []*engines.Message {
-	allMessagesFromAssistant := true
-	for _, msg := range options {
-		if msg.Role != engines.ChatRoleAssistant {
-			allMessagesFromAssistant = false
-			break
-		}
-	}
-
-	if !allMessagesFromAssistant {
-		// no obvious policy to choose from
-		return options
-	}
-
-	prompt := agentState.Settings.Agent.PromptBased.Prompt
-	promptLines := strings.Split(prompt, "\n")
-	initialGoal := promptLines[0]
-	messageRatings := make([]float32, len(options))
-
-	for i, msg := range options {
-		rating, err := agentState.VoteForAction(initialGoal, msg.Content)
-		if err != nil {
-			fmt.Printf("Error voting for action: %v\n", err)
-			continue
-		}
-
-		messageRatings[i] = rating
-		fmt.Printf("Vote for message %d of %d finished with rating: %f\n", i, len(options), rating)
-	}
-
-	// now calculate the average rating
-	averageRating := float32(0.0)
-	for _, rating := range messageRatings {
-		averageRating += rating
-	}
-	averageRating /= float32(len(messageRatings))
-
-	filteredOptions := make([]*engines.Message, 0)
-	for i, msg := range options {
-		if messageRatings[i] >= averageRating {
-			filteredOptions = append(filteredOptions, msg)
-		}
-	}
-
-	return filteredOptions
-}
-
 func deDupeChats(chats [][]*engines.Message) [][]*engines.Message {
 	chatSignatures := make(map[string]struct{})
 	deDupedChats := make([][]*engines.Message, 0)
