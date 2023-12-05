9d513f0134b9f462d7d2e3370d344b33b9d7ab93
ds <7993863@gmail.com>
2023-12-05 02:16:23 +0000

    go a bit deeper and a bit faster in example

--- agency/tunables.go
+++ agency/tunables.go
@@ -2,10 +2,10 @@ package agency
 
 import "time"
 
-const NumberOfVotesToCache = 5
+const NumberOfVotesToCache = 3
 const VoterMinResults = 7
 const MinimumNumberOfVotes = 3
 const MinimalVotingRatingForCommand = 5.0
-const ToTPathLenToTriggerTerminalCallback = 5
+const ToTPathLenToTriggerTerminalCallback = 7
 const ResubmitSystemPromptAfter = 15 * time.Minute
 const MaxIoRequestsThreads = 128
